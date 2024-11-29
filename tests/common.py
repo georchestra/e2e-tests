7f8d0212829b14c3607fd6f6e31612178650c4be
Florian Necas <florian.necas@camptocamp.com>
2024-11-29 14:11:29 +0100

    feat: remove unwated tests, define config, set base url

--- tests/common.py
+++ tests/common.py
@@ -1,4 +1,9 @@
 import allure
+from playwright.sync_api import Page
+
+from helper.configuration_manager import ConfigurationManager
+
+
 
 def screenshot_page(page, name):
     png_bytes = page.screenshot()
@@ -7,3 +12,17 @@ def screenshot_page(page, name):
         name=name,
         attachment_type=allure.attachment_type.PNG
     )
+
+def login(page: Page, cas: bool = False):
+    page.goto("/datahub/")
+    page.get_by_role("link", name="login").click()
+    username_input = page.get_by_placeholder("Username")
+    if cas:
+        username_input = page.locator("#username")
+    username_input.fill(ConfigurationManager.local_admin()["username"])
+    username_input.press("Tab")
+    password_input = page.get_by_placeholder("Password")
+    if cas:
+        password_input = page.locator("#password")
+    password_input.fill(ConfigurationManager.local_admin()["password"])
+    password_input.press("Enter")
--- tests/test_georchestra_apps.py
+++ tests/test_georchestra_apps.py
@@ -3,25 +3,11 @@ import allure
 
 from playwright.sync_api import Page, expect
 
-from common import screenshot_page
+from tests.common import screenshot_page, login
 
-BASE_URL="https://georchestra-127-0-0-1.nip.io"
-LOCAL_ACCOUNT_USERNAME=os.getenv("LOCAL_ACCOUNT_USERNAME")
-LOCAL_ACCOUNT_PASSWORD=os.getenv("LOCAL_ACCOUNT_PASSWORD")
 
-def login(page: Page, cas: bool = False):
-    page.goto(f"{BASE_URL}/datahub/")
-    page.get_by_role("link", name="login").click()
-    username_input = page.get_by_placeholder("Username")
-    if cas:
-        username_input = page.locator("#username")
-    username_input.fill(LOCAL_ACCOUNT_USERNAME)
-    username_input.press("Tab")
-    password_input = page.get_by_placeholder("Password")
-    if cas:
-        password_input = page.locator("#password")
-    password_input.fill(LOCAL_ACCOUNT_PASSWORD)
-    password_input.press("Enter")
+
+
 
 @allure.epic("Web interface")
 @allure.feature("geOrchestra")
@@ -29,7 +15,7 @@ def login(page: Page, cas: bool = False):
 @allure.description("This test attempts to load GeoNetwork.")
 @allure.title("Test the GeoNetwork webapp")
 def test_geo_network_webapp(page: Page):
-    page.goto(f"{BASE_URL}/geonetwork/srv/eng/catalog.search")
+    page.goto("/geonetwork/srv/eng/catalog.search")
     # page.wait_for_timeout(20000)
     screenshot_page(page,"geonetwork")
     expect(page.get_by_role("combobox", name="Search")).to_be_visible()
@@ -40,7 +26,7 @@ def test_geo_network_webapp(page: Page):
 @allure.description("This test attempts to load the 'create account' page from the Console.")
 @allure.title("Test the 'create account' interface from the Console")
 def test_console_create_account(page: Page):
-    page.goto(f"{BASE_URL}/console/account/new")
+    page.goto("/console/account/new")
     screenshot_page(page,"console-createAccount")
     expect(page.get_by_role("heading", name="New account. Create your")).to_be_visible()
 
@@ -50,7 +36,7 @@ def test_console_create_account(page: Page):
 @allure.description("This test attempts to load the 'password recovery' page from the Console.")
 @allure.title("Test the 'password recovery' interface from the Console")
 def test_console_password_recovery(page: Page):
-    page.goto(f"{BASE_URL}/console/account/passwordRecovery")
+    page.goto("/console/account/passwordRecovery")
     screenshot_page(page,"console-passwordRecovery")
     expect(page.get_by_role("heading", name="Ask for a new password. You'")).to_be_visible()
 
@@ -61,7 +47,7 @@ def test_console_password_recovery(page: Page):
 @allure.title("Test the DataFeeder webapp")
 def test_data_feeder_webapp(page: Page):
     login(page)
-    page.goto(f"{BASE_URL}/import/")
+    page.goto("/import/")
     screenshot_page(page,"datafeeder")
     expect(page.locator("ngx-dropzone")).to_be_visible()
 
@@ -71,7 +57,7 @@ def test_data_feeder_webapp(page: Page):
 @allure.description("This test attempts to load the GeoServer main page.")
 @allure.title("Test the GeoServer webapp")
 def test_geo_server_webapp(page: Page):
-    page.goto(f"{BASE_URL}/geoserver/web/")
+    page.goto("/geoserver/web/")
     screenshot_page(page,"geoserver")
     expect(page.get_by_role("heading", name="Welcome")).to_be_visible()
 
@@ -81,41 +67,6 @@ def test_geo_server_webapp(page: Page):
 @allure.description("This test attempts to load the MapStore landing page.")
 @allure.title("Test the MapStore webapp")
 def test_map_store_webapp(page: Page):
-    page.goto(f"{BASE_URL}/mapstore/")
+    page.goto("/mapstore/")
     screenshot_page(page,"mapstore")
     expect(page.locator("canvas")).to_be_visible()
-
-@allure.epic("Web interface")
-@allure.feature("geOrchestra")
-@allure.story("GeoServer")
-@allure.description("This test attempts to open Geoserver's Layer Preview of the public 'arrondissement' layer.")
-@allure.title("Test a GeoServer preview of the 'arrondissement' layer")
-def test_geo_server_refer_sols_layer_preview(page: Page):
-    page.goto(f"{BASE_URL}/geoserver/")
-    page.get_by_role("link", name="Layer Preview Layer Preview").click()
-    page.get_by_placeholder("Search").click()
-    page.get_by_placeholder("Search").fill("arrondissement")
-    page.get_by_placeholder("Search").press("Enter")
-    screenshot_page(page,"gs-layers-preview")
-    page.wait_for_timeout(1000)
-    # page1 = page.wait_for_popup(lambda: page.get_by_role("link", name="OpenLayers").first().click())
-    # assert page1.locator("canvas").is_visible()
-    # page1.screenshot(path="gs-layer-preview")
-
-@allure.epic("Web interface")
-@allure.feature("geOrchestra")
-@allure.story("MapStore")
-@allure.description("This test attempts to load the 'arrondissement' layer into the Mapstore viewer.")
-@allure.title("Test to load the 'arrondissement' layer into the Mapstore viewer")
-def test_mapstore_add_refer_sols_layer(page: Page):
-    page.goto(f"{BASE_URL}/mapstore/")
-    page.locator("#drawer-menu-button").click()
-    page.get_by_role("button", name="").click()
-    page.locator(".Select-arrow").click()
-    page.get_by_label("Service de données de la").click()
-    page.get_by_placeholder("text to search...").click()
-    page.get_by_placeholder("text to search...").fill("arrondissement")
-    page.get_by_placeholder("text to search...").press("Enter")
-    page.locator("#mapstore-metadata-explorer .catalog-results").get_by_role("button").click()
-    screenshot_page(page, "mapstore")
-    expect(page.locator("#mapstore-layers").get_by_text("test")).to_be_visible()
