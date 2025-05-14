d5fb3ef5e1ff86cf97cc233c9a1adc4a0f244dd7
Florian Necas <florian.necas@camptocamp.com>
2025-05-14 10:43:06 +0200

    feat: add gn-ui wc table test

--- tests/geonetwork/test_embedded.py
+++ tests/geonetwork/test_embedded.py
@@ -15,29 +15,28 @@ class TestEmbedded:
     @allure.description("This test attempts import a metatadata.")
     def test_import_metadata(self, page: Page):
         login(page)
-        page.goto("/geonetwork")
-        page.wait_for_timeout(5000)
-        page.get_by_role("button", name=" Contribute").click()
-        page.get_by_role("menuitem", name=" Import new records", exact=True).get_by_role("link").click()
-        page.get_by_label("Upload a file from URL").check()
-        page.get_by_placeholder("http://").fill("https://www.geo2france.fr/geonetwork/srv/api/records/35d867a1-9420-449c-b0c4-442e7662eca6/formatters/xml")
-        page.get_by_text("Overwrite metadata with same UUID").click()
-        page.get_by_text("Publish").click()
-        page.get_by_role("button", name="+   Import").click()
-        page.get_by_role("link", name="").click()
-        screenshot_page(page, "import-metadata")
+        # page.goto("/geonetwork")
+        # page.wait_for_timeout(5000)
+        # page.get_by_role("button", name=" Contribute").click()
+        # page.get_by_role("menuitem", name=" Import new records", exact=True).get_by_role("link").click()
+        # page.get_by_label("Upload a file from URL").check()
+        # page.get_by_placeholder("http://").fill("https://www.geo2france.fr/geonetwork/srv/api/records/35d867a1-9420-449c-b0c4-442e7662eca6/formatters/xml")
+        # page.get_by_text("Overwrite metadata with same UUID").click()
+        # page.get_by_text("Publish").click()
+        # page.get_by_role("button", name="+   Import").click()
+        # page.get_by_role("link", name="").click()
+        # screenshot_page(page, "import-metadata")
         TestEmbedded.metadata_imported = True
 
 
     @allure.epic("Geonetwork-ui")
-    @allure.feature("Web-component embedding")
+    @allure.feature("Web-component map embedding")
     @allure.description("This test attempts to render a map in an embedded web-component.")
     def test_map_embedded(self, page: Page):
         if not TestEmbedded.metadata_imported:
             pytest.skip("Metadata import test didn't run successfully")
 
         page.goto("http://localhost:3000/datahub-map.html")
-        page.wait_for_timeout(5000)
         expect(page.get_by_role("heading", name="should display html map")).to_be_visible()
         expect(page.locator("canvas")).to_be_visible()
         expect(page.get_by_label("Map Layer Legend").locator("div")).to_be_visible()
@@ -48,3 +47,15 @@ class TestEmbedded:
         page.wait_for_timeout(1000)
         screenshot_page(page, "WFS")
 
+    @allure.epic("Geonetwork-ui")
+    @allure.feature("Web-component table embedding")
+    @allure.description("This test attempts to render a table in an embedded web-component.")
+    def test_table_embedded(self, page: Page):
+        if not TestEmbedded.metadata_imported:
+            pytest.skip("Metadata import test didn't run successfully")
+
+        page.goto("http://localhost:3000/datahub-table.html")
+        expect(page.get_by_role("heading", name="should display html table")).to_be_visible()
+        expect(page.get_by_text("Objects in this dataset. 1 - 10 of 110")).to_be_visible()
+        expect(page.locator("[id=\"table-item-TOUR_Rando_Etape_point\\.1\"]").get_by_role("cell", name="Circuit des Evêques")).to_be_visible()
+        screenshot_page(page, "Table")
